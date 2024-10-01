18730349801fc137e9e87eec52cf5acf4ef98792
Timur Khusnutdinov <timur.husnutdinov@novakidschool.com>
2024-10-01 13:21:01 +0300

    init template

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const KeyCode = {
+  BACKSPACE: 8,
+  ENTER: 13,
+}
+
+export const MAX_GUESSES = 6;
+export const WORD_LENGTH = 5;
+
+// Define the fixed solution word here
+export const SOLUTION = "APPLE";
